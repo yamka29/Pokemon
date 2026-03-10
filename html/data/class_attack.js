07937e8c432d3cf6ac6d1d9ec114d2e9076de187
yamka29 <kadriyiwen543@gmail.com>
2026-03-10 16:07:28 +0100

    créé classe Attack

---
+++ html/data/class_attack.js
@@ -0,0 +1,9 @@
+class Attack{
+    constructor(id, n, t, p, d){
+        this.id_attack = id;
+        this.name = n;
+        this.type = t;
+        this.power = p;
+        this.duration = d;
+    }
+}
