cbeddaf8ce87e5036c35dd4446f92c99a0c2a8b9
kaelig HILAIRE <k.hilaire@orange.fr>
2026-03-18 08:47:22 +0100

    modifications pour tests

--- html/data/class_type.js
+++ html/data/class_type.js
@@ -2,3 +2,22 @@ import "pokemon_moves.js";
 import "fast_moves.js";
 import "charged_moves.js";
 
+class Type {
+    constructor(nom) {
+        this.nom = nom;
+    }
+
+    toString() {
+
+        for (let index = 0; index < type_effectiveness.length(); index++) {
+            const element = array[index];
+            
+        }
+
+        console.log(element);
+
+        if (this.nom) {
+            
+        }
+    }
+}
