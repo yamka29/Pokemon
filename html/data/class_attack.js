25ddf0f5853a8bb3e0b58342cbb0de9771d176d7
yamka29 <kadriyiwen543@gmail.com>
2026-03-18 08:54:17 +0100

    ajouté jquery

--- html/data/class_attack.js
+++ html/data/class_attack.js
@@ -1,3 +1,8 @@
+import "pokemon_moves.js";
+import "fast_moves.js";
+import "charged_moves.js";
+
+
 class Attack{
     constructor(id, n, t, p, d){
         this.id_attack = id;
@@ -6,4 +11,15 @@ class Attack{
         this.power = p;
         this.duration = d;
     }
+
+    toString(){
+       return `${this.nom} : 
+       #${this.id_attack}, 
+       ${this.type},
+       ${this.power},
+       ${this.duration}ms` 
+    }
 }
+
+let Charge = new Attack(12, "Tackle", Normal, 5, 50);   
+console.log(Charge.toString);
