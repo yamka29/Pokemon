cc88e7e6524bca93dd9f0a9c0e82ea0b07116ba6
kaelig HILAIRE <k.hilaire@orange.fr>
2026-03-25 12:06:40 +0100

    commentaires de la classe type

--- html/data/class_type.js
+++ html/data/class_type.js
@@ -1,6 +1,9 @@
 export class Type {
 
+    // Données statiques contenant les informations sur l'efficacité des types
     static data = type_effectiveness;
+    
+    // Tableau statique contenant toutes les instances de Type
     static all_types = Object.keys(type_effectiveness).map(typeName => new Type(typeName));
 
     constructor(nom) {
@@ -14,17 +17,21 @@ export class Type {
             var tab = Type.data[this.nom];
             console.table(tab); 
             
+            // Parcourir les données d'efficacité et grouper les types par valeur
             for (let nomType in tab){
                 const value = tab[nomType];
                 console.log("nomType : " + nomType + " value : " + value);
                 
+                // Créer une clé dans groupes si elle n'existe pas
                 if (!groupes[value]) {
                     groupes[value] = [];
                 }
                 
+                // Ajouter le type au groupe correspondant
                 groupes[value].push(nomType);
             }
             
+            // Construire la chaîne de résultat
             let str = this.nom + " : ";
 
             for (let value in groupes) {
@@ -38,6 +45,10 @@ export class Type {
         }
     }
 
+    /**
+     * Ajoute un nouveau type à la liste statique all_types
+     * @param {string} nom - Le nom du type Pokémon à ajouter
+     */
     fill_types(nom) {
         let nType = new Type(nom);
         Type.all_types.push(nType);
