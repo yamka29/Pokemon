6e8a5c6fb70a3f482c508b809d3ec483ead7e41b
yamka29 <kadriyiwen543@gmail.com>
2026-03-29 20:15:25 +0200

    ajout des deux fonctions de tests pour trouver un pokemon par son type et par une attaque

--- html/test/test.js
+++ html/test/test.js
@@ -5,12 +5,40 @@ fill_pokemons();
 
 //fonction utilitaire qui renvoie une liste d'objets (Attaques ou Pokemons) formatée
 function displayList(elems, type_elems){
-    console.log(`Liste des ${elems.length} ${type_elems} : `);
-    for (let elem in elems){
-        console.log(`- ${elem.toString()}`);
+    let list = `Liste des ${elems.length} ${type_elems} : \n`;
+    for (let elem of elems){
+        list += `- ${elem.toString()} \n`;
     }
+
+    return list;
 }
 
+//fonction affichant dans la console, la liste des pokémons pour un type donné en paramètre
+function getPokemonsByType(typeName){
+    let pokemonsOfThisType = Object.values(Pokemon.all_pokemons).filter(    
+        pokemon => pokemon.types.some(
+            type => type.name.toLowerCase() === typeName.toLowerCase()
+        )
+    );
+
+    return displayList(pokemonsOfThisType, "Pokémons");
+
+}
+// Fonction affichant la liste des pokémons possédant l'attaque en paramètre
+function getPokemonsByAttack(attackName){
+    let pokemonWithThisAttack = Object.values(Pokemon.all_pokemons).filter(
+        pokemon => pokemon.fast_attacks.concat(pokemon.charged_attacks).some(
+            attack => attack.name.toLowerCase() === attackName.toLowerCase()
+        )
+    )
+
+    return displayList(pokemonWithThisAttack, "Pokémons");
+}
+
+console.log("-------------------");
+console.log("Pokemons with the move Razor Leaf");
+console.log(getPokemonsByAttack('Razor Leaf'))
+console.log("-------------------");
 // console.log(Attack.all_attacks[221].toString());
 // console.log(Type.all_types["Bug"].toString());
 // console.log(Pokemon.all_pokemons[1].toString());
