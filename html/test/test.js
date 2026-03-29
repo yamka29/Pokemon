9a1a22e2d32ed40f2cce5e5028fae43debbb8882
yamka29 <kadriyiwen543@gmail.com>
2026-03-29 19:27:58 +0200

    ajout d'une methode pour le formatage des tests

--- html/test/test.js
+++ html/test/test.js
@@ -1,8 +1,16 @@
+//Fonctions pour remplir les vartiables de classes nécessaires aux tests
 fill_types();
 fill_attacks();
 fill_pokemons();
 
+//fonction utilitaire qui renvoie une liste d'objets (Attaques ou Pokemons) formatée
+function displayList(elems, type_elems){
+    console.log(`Liste des ${elems.length} ${type_elems} : `);
+    for (let elem in elems){
+        console.log(`- ${elem.toString()}`);
+    }
+}
 
-console.log(Attack.all_attacks[221].toString());
-console.log(Type.all_types["Bug"].toString());
-console.log(Pokemon.all_pokemons[1].toString());
+// console.log(Attack.all_attacks[221].toString());
+// console.log(Type.all_types["Bug"].toString());
+// console.log(Pokemon.all_pokemons[1].toString());
---
+++ html/test/verif.js
@@ -0,0 +1,15 @@
+console.log("Tous les Pokemons");
+console.log(Pokemon.all_pokemons);
+console.log("-------------------");
+console.log("Pokemon 1 (Bulbasaur)");
+console.log(Pokemon.all_pokemons[1].toString());
+console.log("-------------------");
+console.log("Pokemons Grass");
+console.log(getPokemonsByType('Grass'))
+console.log("-------------------");
+console.log("Attaque 210 (Wing Attack)");
+console.log(Attack.all_attacks[210].toString());
+console.log("-------------------");
+console.log("Type Bug");
+console.log(Type.all_types['Bug'].toString());
+console.log("-------------------");
